ae9d0e2de732ac94a2721d612e1e2523c605dd28
신희성 <siniseong@amuz.co.kr>
2025-08-14 17:31:55 +0900

    feat :: vue + tailwindcss 환경 설정

--- step3-web-ui/Client/vite.config.js
+++ step3-web-ui/Client/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    postcss: './postcss.config.cjs',
+  },
+  server: {
+    port: 5173,
+    host: true
+  }
 })
