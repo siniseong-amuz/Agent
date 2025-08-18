cffaf6060310d1b0c28e7466d8befc4bd93cb408
신희성 <siniseong@amuz.co.kr>
2025-08-18 11:10:53 +0900

    chore :: 메인 글자 그라디언트 적용

--- step3-web-ui/Client/src/App.vue
+++ step3-web-ui/Client/src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="min-h-screen bg-white flex items-center justify-center">
-    <h1 class="text-2xl text-black">안녕하세요</h1>
+  <div class="min-h-screen bg-[#1E1E1E] flex items-center justify-center flex-col">
+    <h1 class="text-4xl font-medium mb-2 gradient-text">안녕하세요, siniseong님</h1>
+    <h1 class="text-4xl font-medium text-[#474747]">무엇을 도와드릴까요?</h1>
   </div>
 </template>
 
