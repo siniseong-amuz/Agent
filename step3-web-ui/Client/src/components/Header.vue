c45fe5f9b33f129f3083a5abfdaef8bc0c9cc044
신희성 <siniseong@amuz.co.kr>
2025-08-19 17:02:16 +0900

    chore :: 헤더 타이틀 변경

--- step3-web-ui/Client/src/components/Header.vue
+++ step3-web-ui/Client/src/components/Header.vue
@@ -10,14 +10,14 @@
           <path d="M3.75 5.25h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1 0-1.5Zm0 6h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1 0-1.5Zm0 6h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1 0-1.5Z"/>
         </svg>
       </button>
-      <div class="absolute inset-0 flex items-center justify-center text-[var(--text)] text-lg">title</div>
-      
+      <div class="absolute inset-0 flex items-center justify-center text-[var(--text)] text-lg">
+        {{ title }}
+      </div>
     </div>
   </header>
 </template>
 
 <script setup>
-
 defineProps({
   isMobile: {
     type: Boolean,
@@ -26,8 +26,12 @@ defineProps({
   isDark: {
     type: Boolean,
     default: false
+  },
+  title: {
+    type: String,
+    default: 'New Chat'
   }
-})
+});
 
-defineEmits(['toggle-sidebar', 'toggle-theme'])
+defineEmits(['toggle-sidebar', 'toggle-theme']);
 </script>
